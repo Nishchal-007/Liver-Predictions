e5265152f5b8f515602313d7a2fe6f2bf2a88b2a
Nishchal-007 <letscollabtogether10@gmail.com>
2021-10-30 12:16:17 +0530

    Started Project

---
+++ server.py
@@ -0,0 +1,17 @@
+from flask import Flask, flash, request, redirect, url_for, render_template, send_file
+import os
+import warnings
+warnings.filterwarnings("ignore")
+
+PEOPLE_FOLDER = os.path.join('static','images')
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'secret-key-goes-here'
+
+@app.route("/", methods=['GET'])
+def index():
+    return render_template('home.html')
+
+#JUST DO IT!!!
+if __name__=="__main__":
+    app.run(port="9000")
